dd8fe5e6ba7da533c60b5135105d1d500ea914cd
devmehmetemre <haslooytr@gmail.com>
2026-09-21 22:27:28 +0300

    Add files via upload

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 # CHA OS Windows derleme yardımcısı (2 aşama: rootfs host'ta, ISO Alpine Docker'da)
 docker build -t chaos-builder -f Dockerfile.builder .
 docker run --rm -v "${PWD}:/work" -w /work chaos-builder sh ./build.sh --arch x86_64 --desktop xfce --version dev
-docker run --rm -v "${PWD}:/work" -w /work alpine:3.22 sh tools/make-iso.sh --arch x86_64 --desktop xfce --version dev
+docker run --rm -v "${PWD}:/work" -w /work alpine:3.22 sh -c "sh tools/make-iso.sh --arch x86_64 --desktop xfce --version dev && chmod -R a+rX /work/out"
 Write-Host "Cikti: out/"
